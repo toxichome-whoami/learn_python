0d6e94693f0b9b7a3d9cfbfa2722007c07fe8fa7
Toxic_Home <toxic.home.whoami@hotmail.com>
2024-03-28 22:41:22 +0600

    Uploaded video 10 source code

---
+++ video 10/main.py
@@ -0,0 +1,14 @@
+# name = str(input("Enter your name: "))
+# old = str(input("How old are you: "))
+
+# print(f"\nMy name is {name} and I am {old} years old.")
+
+num_1 = float(input("Enter your 1st number: "))
+num_2 = float(input("Enter your 2nd number: "))
+
+print(
+    f" The addition of {num_1} and {num_2} is: {num_1 + num_2}\n",
+    f"The subtraction of {num_1} and {num_2} is: {num_1 - num_2}\n",
+    f"The multiplication of {num_1} and {num_2} is: {num_1 * num_2}\n",
+    f"The division of {num_1} and {num_2} is: {num_1 / num_2}",
+)
