f95ca18e80cfe6f9d8d7ac0e5ef49c50c3f2a3fd
Toxic_Home <toxic.home.whoami@hotmail.com>
2024-03-28 21:52:30 +0600

    Uploaded video 8 source code

---
+++ video 8/main.py
@@ -0,0 +1,9 @@
+num_1 = 15
+num_2 = 5
+
+print (
+    f"The addition of {num_1} and {num_2} is: {num_1 + num_2}\n",
+    f"The subtraction of {num_1} and {num_2} is: {num_1 - num_2}\n",
+    f"The multiplication of {num_1} and {num_2} is: {num_1 * num_2}\n",
+    f"The division of {num_1} and {num_2} is: {num_1 / num_2}"
+)
