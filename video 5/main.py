d44e90abd5479002acd73529b21c1b6a5824be44
Toxic_Home <toxic.home.whoami@hotmail.com>
2024-03-28 20:55:32 +0600

    Uploaded video 5 source code

---
+++ video 5/main.py
@@ -0,0 +1,15 @@
+"""
+Hey Tasbir, don't remove this line of code
+Author : Tasbir
+Course : 100 Days Of Code
+"""
+
+print('"Hey I am a good boy".\nThis viewer is also a good boy/girl')
+
+print("Hello", 6, 7, sep="~", end="009\n")
+print("Tasbir")
+
+# print("Hello, World!!!")
+# # This is a 'Single-line Comment'
+# print("This is a print statement")
+# print("Hello, World!!!")  # printing Hello, World!!!
