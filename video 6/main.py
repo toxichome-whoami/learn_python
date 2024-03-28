fe14fb069d7ac340ab76bdd48bbd5196837d4786
Toxic_Home <toxic.home.whoami@hotmail.com>
2024-03-28 21:28:25 +0600

    Uploaded video 6 source code

---
+++ video 6/main.py
@@ -0,0 +1,24 @@
+# a = 1414235235236436
+# print(a)
+
+# b = "Tasbir"
+# print(b)
+
+a = 1
+b = True
+c = "Tasbir"
+d = None
+
+print(
+    f"The Data type of a is: {type(a)}\n",
+    f"The Data type of b is: {type(b)}\n",
+    f"The Data type of c is: {type(c)}\n",
+    f"The Data type of d is: {type(d)}",
+)
+
+print("\n")
+
+var_a = 34
+var_b = 45
+print(f"The addition of {var_a} and {var_b} is: {var_a + var_b}")
+
